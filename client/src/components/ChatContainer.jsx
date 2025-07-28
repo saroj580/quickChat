24493790ebc07d06ba9a313a69cc7ef80300aa80
saroj580 <chaudharysaroj819@gmail.com>
2025-07-28 16:59:58 +0530

    create rightsidebar

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -54,7 +54,7 @@ const ChatContainer = () => {
             <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
                 <img src={selectedUser.profilePic || assets.avatar_icon} alt="Profile" className="w-8 rounded-full" />
                 <p className="flex-1 text-lg text-white flex items-center gap-2">{selectedUser.fullName}
-                    {onlineUsers.includes(selectedUser._id)} <span className="w-2 h-2 rounded-full bg-green-500"></span>
+                    {onlineUsers.includes(selectedUser._id) && <span className="w-2 h-2 rounded-full bg-green-500"></span>}
                 </p>
                 <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="Arrow" className="md:hidden max-w-7"/>
                 <img src={assets.help_icon } className="max-md:hidden max-w-5" />
